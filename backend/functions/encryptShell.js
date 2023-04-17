698727ad4da31a4c6b0b78f22c915aa5059dd8d2
Keerthi Vibisan - Linux <trimaqua7760@gmail.com>
2023-04-17 10:40:12 +0530

    encryption of text added and the encrypted text sent to frontend

--- backend/functions/encryptShell.js
+++ backend/functions/encryptShell.js
@@ -1,16 +1,21 @@
 const { exec } = require('child_process');
 
-function encryptShell(command) {
+function encryptShell(text, isKey, key) {
     return new Promise((resolve, reject) => {
-        exec('dir /S', (err, stdout, stderr) => {
-            if (err) {
-              console.error(`exec error: ${err}`);
-                reject();
-            }
-            console.log(`stdout: ${stdout}`);
-            console.log(`stderr: ${stderr}`);
-            resolve(stdout);
-          });
+      let cmd = `echo -d "${text}" | openssl enc -e -aes-256-cbc -a -salt -k 123456789987654321 -iv aaabbbccc`;  
+      if(isKey == true) {
+        cmd = `echo -d "${text}" | openssl enc -e -aes-256-cbc -a -salt -k ${key} -iv aaabbbccc`;
+      }
+      // console.log(cmd);
+      exec(cmd, (err, stdout, stderr) => {
+          if (err) {
+            console.log(`exec error: ${err}`);
+            reject(err);
+          }
+          // console.log(`stdout: ${stdout}`);
+          // console.log(`stderr: ${stderr}`);
+          resolve(stdout);
+        });
     })
 }
 
