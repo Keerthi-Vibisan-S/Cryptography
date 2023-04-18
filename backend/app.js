42c4e263e54a333823002eafffb5e3e7b99bc5b5
Keerthi Vibisan <trimaqua7760@gmail.com>
2023-04-18 16:20:49 +0530

    Basic Socket Code added

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const cors = require("cors");
 
+const http = require('http');
+const { Server } = require('socket.io');
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://192.168.168.220:3000",
+        methods: ["GET", "POST"]
+    }
+});
 
 app.get("/", (req, res) => {
     res.send("Hello, what are you looking for");
@@ -19,7 +31,14 @@ app.use("/decrypt", decrypt);
 const err = require('./error/api-error-handler');
 app.use(err);
 
+// Socket Chat
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+})
+
+
 const port = process.env.PORT || 3001;
-app.listen(port, () => {
+server.listen(port, () => {
     console.log("Listening on Port ",port);
 })
