2c20708551aee14e23d38bfda17c8128c1192387
Keerthi Vibisan <trimaqua7760@gmail.com>
2023-05-05 15:05:07 +0530

    ⚠️  app.js socket access origin changed to netlify

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        //origin: "http://localhost:3000",
+        origin: "https://cryptography.netlify.app",
         methods: ["GET", "POST"]
     }
 });
